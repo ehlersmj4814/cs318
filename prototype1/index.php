a3cce36ac92a015c6079ec2762b6049fdf2a32b9
ehlersmj4814 <ehlersmj4814@uwec.edu>
2017-04-12 14:12:11 -0500

    lab18

---
+++ prototype1/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Heros of the Storm Database | Home</title>
+        <link href="style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <header></header>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Heros</li>
+                <li>Maps</li>
+                <li>Other Data</li>
+            </ul>
+        </nav>
+        <main>
+            <img>
+            <h1>Heros of the Storm Database</h1>
+            <p></p>
+            <h2>News Feed</h2>
+        </main>
+        <footer></footer>
+    </body>
+</html>
