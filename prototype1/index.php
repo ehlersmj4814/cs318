5dcfc204bb8500b21d7a2290657480b45b70e349
ehlersmj4814 <ehlersmj4814@uwec.edu>
2017-04-26 12:28:06 -0500

    Add files via upload

--- prototype1/index.php
+++ prototype1/index.php
@@ -3,23 +3,18 @@
     <head>
         <title>Heros of the Storm Database | Home</title>
         <link href="style.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     </head>
     <body>
-        <header></header>
-        <nav>
-            <ul>
-                <li>Home</li>
-                <li>Heros</li>
-                <li>Maps</li>
-                <li>Other Data</li>
-            </ul>
-        </nav>
-        <main>
-            <img>
-            <h1>Heros of the Storm Database</h1>
-            <p></p>
-            <h2>News Feed</h2>
-        </main>
-        <footer></footer>
+            <?php include 'navigation.php'; ?>
+            <main>
+                <h1>Heroes of the Storm Database</h1>
+                <p>The purpose of this app is to provide accurate and up to date information on the heroes, maps, and other aspects of Blizzard's Heroes of the Storm. [Insert information on where to go next]</p>
+                <h2 id="news-head">News Feed</h2>
+                <div class="news" id="news1"><div class="name">Heroes 2.0</div></div>
+                <div class="news" id="news2"><div class="name">New Hero!</div></div>
+                <div class="news" id="news3"><div class="name">New Battleground!</div></div>
+            </main>
+        <?php include 'footer.php'; ?>
     </body>
 </html>
