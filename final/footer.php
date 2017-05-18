59121a83081c1cb611e0759cca613c4a752516a3
ehlersmj4814 <ehlersmj4814@uwec.edu>
2017-05-18 16:49:51 -0500

    update footer

--- final/footer.php
+++ final/footer.php
@@ -4,7 +4,7 @@
         <div id=\"footer-right\"><h2>Contact Us</h2>
         <a href=\"mailto:info@meinen.io?subject=Hots%20Database%20Inquiry\" target=\"_top\">info@meinen.io</a></div>
         <p>If you have any updates to suggest for this site, please consider getting in touch with me. If there are any updates to specific heroes, please submit a request on <a href=\"update.php\">this</a> page.</p>
-        <p>Content is available under <a href=\"https://creativecommons.org/licenses/by-nc-sa/3.0/\">CC BY-NC-SA 3.0</a> unless otherwise noted.<br>
+        <p>Content is available under <a href=\"https://creativecommons.org/licenses/by-nc-sa/3.0/\" target=\"_blank\">CC BY-NC-SA 3.0</a> unless otherwise noted.<br>
         Heroes of the Storm and materials are trademarks and copyrights of Blizzard and its licensors. All rights reserved. This site is not affiliated with Blizzard.</p>
     </footer>";
 ?>
